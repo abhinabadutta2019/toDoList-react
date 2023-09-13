c7952731c7c78ecb5e801c463e2a80a6f20d7a5d
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-09-13 07:19:14 +0530

    testing button, onClick function

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,24 @@
 const Home = () => {
+  //
+  const handleClick = (name) => {
+    console.log("hello world" + name);
+  };
+  //
+  const handleClickAgain = (name) => {
+    console.log("hello world " + name);
+  };
+  //
   return (
     <div>
-      <h2>Homepage</h2>
+      <h2>Home</h2>
+      <button onClick={handleClick("mario")}>Click here</button>
+      <button
+        onClick={(e) => {
+          handleClickAgain("Virat");
+        }}
+      >
+        Click for more
+      </button>
     </div>
   );
 };
