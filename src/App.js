33d751bb12311548a8edb376e90c84f64df8bc1e
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-09-13 06:33:36 +0530

    creating basic component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
+import Nested1 from "./Nested1";
 
 function App() {
   //
-
+  const title = "To do list app component";
   //
   return (
     <div className="App">
-      <div className="Component">
-        <h2>Basic setup</h2>
+      <div>
+        <h2>{title}</h2>
+
+        <Nested1 />
       </div>
     </div>
   );
---
+++ src/Nested1.js
@@ -0,0 +1,9 @@
+const Nested1 = () => {
+  return (
+    <div className="Nested1">
+      <p>Hi from nested1</p>
+    </div>
+  );
+};
+
+export default Nested1;
