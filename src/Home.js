56724d395bea1a6f644e1ce1e80667538c51d775
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-09-16 13:46:43 +0530

    declaring winner part

--- src/Home.js
+++ src/Home.js
@@ -3,16 +3,66 @@ import TaskList from "./TaskList";
 
 const Home = () => {
   //
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  // console.log(squares, "squares");
   //
   function handleClick(i) {
+    //
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    //
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    //
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    //
+    setXIsNext(!xIsNext);
     setSquares(nextSquares);
   }
   //
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  //
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  //
+  //
   return (
     <>
+      <div className="status">{status}</div>
       <div>
         <TaskList value={squares[0]} onSquareClick={() => handleClick(0)} />
         <TaskList value={squares[1]} onSquareClick={() => handleClick(1)} />
