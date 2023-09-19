3c65adaae5de9d5670fb938bebc3c44029d1af51
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-09-19 23:10:34 +0530

    this worked for list of lists

--- src/Home.js
+++ src/Home.js
@@ -2,29 +2,24 @@ import { recipes } from "./TaskList.js";
 
 export default function RecipeList() {
   //
-  const mapValue = recipes.map((item) => {
-    //
-    let emptyArray = [];
-    const ingredientsMap = item.ingredients.forEach((oneIng) => (
-      // console.log(oneIng)
-      <li key={oneIng}>{oneIng}</li>
-    ));
 
-    // console.log(ingredientsMap);
-
-    //
-    return (
-      <div>
-        {item.name}
-        {ingredientsMap}
-      </div>
-    );
-  });
   //
   return (
     <div>
       <h1>Recipes</h1>
-      {mapValue}
+
+      <div>
+        {recipes.map((oneItem) => {
+          return (
+            <div key={oneItem.id}>
+              {oneItem.name}
+              {oneItem.ingredients.map((ing) => {
+                return <li key={ing}>{ing}</li>;
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
