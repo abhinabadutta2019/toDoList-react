be2f9601ce9528924dbcd1ad58609f4a1d94fc2a
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-09-20 18:21:37 +0530

    test

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,3 +1,20 @@
-export function getImageUrl(imageId) {
-  return "https://i.imgur.com/" + imageId + "s.jpg";
+import Profile from "./Profile.js";
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        person={{
+          imageId: "lrWQx8l",
+          name: "Subrahmanyan Chandrasekhar",
+        }}
+      />
+      <Profile
+        person={{
+          imageId: "MK3eW3A",
+          name: "Creola Katherine Johnson",
+        }}
+      />
+    </>
+  );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,30 @@
-import { recipes } from "./TaskList.js";
+import Panel from "./Panel.js";
+import { getImageUrl } from "./utils.js";
 
-export default function RecipeList() {
-  //
+let currentPerson;
 
-  //
+export default function Profile({ person }) {
+  currentPerson = person;
   return (
-    <div>
-      <h1>Recipes</h1>
+    <Panel>
+      <Header />
+      <Avatar />
+    </Panel>
+  );
+}
 
-      <div>
-        {recipes.map((oneItem) => {
-          return (
-            <div key={oneItem.id}>
-              {oneItem.name}
-              {oneItem.ingredients.map((ing) => {
-                return <li key={ing}>{ing}</li>;
-              })}
-            </div>
-          );
-        })}
-      </div>
-    </div>
+function Header() {
+  return <h1>{currentPerson.name}</h1>;
+}
+
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src={getImageUrl(currentPerson)}
+      alt={currentPerson.name}
+      width={50}
+      height={50}
+    />
   );
 }
