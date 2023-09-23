afb40a87660c7279c56c490119f4d110b9ca288f
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-09-23 21:48:53 +0530

    trying conditional with color change with css

--- src/Home.js
+++ src/Home.js
@@ -20,7 +20,13 @@ const Home = () => {
       <p>{value}</p>
       <button onClick={eventHandler}>change button</button>
       {/* using conditional, ternery and useState method */}
-      <p>{value ? <>stay</> : <>go</>}</p>
+      <h3>
+        {value ? (
+          <span style={{ color: "darkred" }}>stay</span>
+        ) : (
+          <span style={{ color: "darkgreen" }}>go</span>
+        )}
+      </h3>
     </div>
   );
 };
