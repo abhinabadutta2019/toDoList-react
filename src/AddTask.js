96fb02176f6d2a0413589242b6e9898cc22ed9a1
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-09-25 19:24:23 +0530

    object +usestate

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,24 +1,35 @@
-export function getFinalState(baseState, queue) {
-  let finalState = baseState;
-
-  console.log(queue, "queue");
-
-  for (let i of queue) {
-    if (typeof i === "function") {
-      // Apply the updater function.
-      finalState = i(finalState);
-      //
-      console.log(i(finalState), "update(finalState)");
-    } else {
-      //   // Replace the next state.
-      finalState = i;
-
-      //   console.log(update, "update");
-    }
-    //
-    // console.log(i, "update");
-    // console.log(typeof update, "typeof update");
-  }
-
-  return finalState;
+import { useState } from "react";
+export default function MovingDot() {
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+  return (
+    <div
+      onPointerMove={(e) => {
+        setPosition({
+          x: e.clientX,
+          y: e.clientY,
+        });
+      }}
+      style={{
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "red",
+          borderRadius: "50%",
+          transform: `translate(${position.x}px, ${position.y}px)`,
+          left: -10,
+          top: -10,
+          width: 20,
+          height: 20,
+        }}
+      />
+    </div>
+  );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,49 +1,44 @@
-import { getFinalState } from "./AddTask.js";
+import { useState } from "react";
 
-function increment(n) {
-  return n + 1;
-}
-increment.toString = () => "n => n+1";
+const Home = () => {
+  const [object, setObject] = useState({ a: "Abhi", b: "Naba", c: "dutta" });
 
-export default function App() {
+  //
+  const eventHandlerA = (e) => {
+    setObject({ ...object, a: e.target.value });
+  };
+
+  //
+  const eventHandlerB = (e) => {
+    setObject({ ...object, b: e.target.value });
+  };
+  //
+  const eventHandlerC = (e) => {
+    setObject({ ...object, c: e.target.value });
+    // console.log("Hi");
+  };
+  //
   return (
     <>
-      <TestCase baseState={0} queue={[1, 1, 1]} expected={1} />
-      <hr />
-      <TestCase
-        baseState={0}
-        queue={[increment, increment, increment]}
-        expected={3}
-      />
-      <hr />
-      <TestCase baseState={0} queue={[5, increment]} expected={6} />
-      <hr />
-      <TestCase baseState={0} queue={[5, increment, 42]} expected={42} />
-    </>
-  );
-}
+      <label>
+        A:
+        <input value={object.a} onChange={eventHandlerA} />
+      </label>
+      <label>
+        B:
+        <input value={object.b} onChange={eventHandlerB} />
+      </label>
+      <label>
+        C:
+        <input value={object.c} onChange={eventHandlerC} />
+      </label>
 
-function TestCase({ baseState, queue, expected }) {
-  const actual = getFinalState(baseState, queue);
-  return (
-    <>
-      <p>
-        Base state: <b>{baseState}</b>
-      </p>
-      <p>
-        Queue: <b>[{queue.join(", ")}]</b>
-      </p>
-      <p>
-        Expected result: <b>{expected}</b>
-      </p>
-      <p
-        style={{
-          color: actual === expected ? "green" : "red",
-        }}
-      >
-        Your result: <b>{actual}</b> (
-        {actual === expected ? "correct" : "wrong"})
-      </p>
+      {/*  */}
+      <p>{object.a}</p>
+      <p>{object.b}</p>
+      <p>{object.c}</p>
     </>
   );
-}
+};
+
+export default Home;
