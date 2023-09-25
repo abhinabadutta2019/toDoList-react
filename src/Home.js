77f23dc635a22378b75ad81a1643a4200eb12a6b
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-09-25 20:44:50 +0530

    changing - object of object with state, basic syntax

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,30 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [value, setValue] = useState({ x: "Abhi", y: "naba" });
+  const [value, setValue] = useState({ x: "Abhi", y: { ya: "AA", yb: "BB" } });
   //
   const eventHandlerX = () => {
     //
     setValue({ ...value, x: "Addd" });
   };
-  //
-  const eventHandlerY = () => {
-    setValue({ ...value, y: "vdddd" });
+  // //
+  const eventHandlerYA = () => {
+    console.log(value.y.ya);
+    setValue({ ...value, y: { ...value.y, ya: "YAYAYA" } });
   };
-  //
-  const eventHandlerBoth = () => {
-    setValue({ y: "xxxx", x: "yyyy" });
-  };
+  // //
+  // const eventHandlerBoth = () => {
+  //   setValue({ y: "xxxx", x: "yyyy" });
+  // };
   //
   return (
     <>
       <button onClick={eventHandlerX}>change x</button>
-      <button onClick={eventHandlerY}>change y</button>
-      <button onClick={eventHandlerBoth}>change Both</button>
+      <button onClick={eventHandlerYA}>change ya</button>
+      {/* <button onClick={eventHandlerBoth}>change Both</button> */}
       <p>x:{value.x}</p>
-      <p>y:{value.y}</p>
+      <p>y.ya:{value.y.ya}</p>
+      <p>y.yb:{value.y.yb}</p>
     </>
   );
 };
