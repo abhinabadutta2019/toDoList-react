3c5af655898a0fbe1bf1bfc9660a475eea16b9a6
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-09-29 23:01:19 +0530

    adding extra key to objects

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,15 @@ import TaskList from "./TaskList";
 
 const Home = () => {
   const initialTodos = [
-    { id: 0, title: "Buy milk", done: true },
-    { id: 1, title: "Eat tacos", done: false },
-    { id: 2, title: "Brew tea", done: false },
+    { id: 0, title: "Buy milk", done: true, inputValue: "" },
+    { id: 1, title: "Eat tacos", inputValue: "" },
+    { id: 2, title: "Brew tea", inputValue: "" },
   ];
 
   const [todos, setTodos] = useState(initialTodos);
   //
-  const [buttonMap, setButtonMap] = useState(todos);
+  // const [buttonMap, setButtonMap] = useState(todos);
+  //
 
   //
   const inputEvent = (e, todoId) => {
@@ -20,19 +21,34 @@ const Home = () => {
     const mapValue = todos.map((todo) => {
       if (todo.id == todoId) {
         console.log(todo.id, todoId);
-        return { ...todo, title: e.target.value };
+        return { ...todo, inputValue: e.target.value };
       } else {
         return todo;
       }
     });
-    setButtonMap(mapValue);
+    // setButtonMap(mapValue);
+
+    setTodos(mapValue);
+
+    console.log(todos, "todos");
+
+    // console.log(mapValue, "mapValue");
     // console.log(buttonClick, "buttonClick");
   };
   //
   const buttonEvent = () => {
-    setTodos(buttonMap);
-  };
+    // setTodos(buttonMap);
 
+    //
+    const mapValue = todos.map((todo) => {
+      if (todo.title != todo.inputValue && todo.inputValue.length > 0) {
+        return { ...todo, title: todo.inputValue };
+      } else {
+        return todo;
+      }
+    });
+    setTodos(mapValue);
+  };
   //
   // const
 
