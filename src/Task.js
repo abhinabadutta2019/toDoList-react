b4cc4c06af0d117bdcb0e4451f1d5f41e4686279
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-01 00:00:28 +0530

    working on update

--- src/Task.js
+++ src/Task.js
@@ -1,7 +1,48 @@
+import { useState } from "react";
+
 const Task = ({ todo, deleteFunc }) => {
+  const [isEditing, setisEditing] = useState(false);
+  const [updateInputValue, setUpdateInputValue] = useState("");
+  //
+
+  const editHandler = () => {
+    setisEditing(true);
+  };
+  //
+  const updateInput = (e) => {
+    setUpdateInputValue(e.target.value);
+  };
+  //
+  const submitUpdate = () => {
+    console.log(updateInputValue);
+  };
+  //
+
+  let content;
+
+  if (isEditing) {
+    // console.log("Hi");
+
+    content = (
+      <>
+        <input onChange={updateInput} />
+        <button onClick={submitUpdate}>save update</button>
+      </>
+    );
+  } else {
+    content = (
+      <>
+        {todo.title}
+        <button onClick={editHandler}>edit</button>
+      </>
+    );
+  }
+
+  //
   return (
+    //
     <>
-      {todo.title}
+      {content}
       <button
         onClick={() => {
           deleteFunc(todo);
