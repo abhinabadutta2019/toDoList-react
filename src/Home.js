d50e7d1e50d758cd97a42004a0be9b388559503d
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-02 18:21:24 +0530

    input field, button, quiz  my try from scratch

--- src/Home.js
+++ src/Home.js
@@ -3,63 +3,36 @@ import { useState } from "react";
 const Home = () => {
   //
   const [answer, setAnswer] = useState("");
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState("typing");
   //
-  if (status === "success") {
-    return <h1>That's right!</h1>;
-  }
-
-  //
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setStatus("submitting");
-    //
-    try {
-      await submitForm(answer);
-      setStatus("success");
-    } catch (err) {
-      setStatus("typing");
-      setError(err);
-    }
-  }
-  //
-  function handleTextareaChange(e) {
+  const answerHandler = (e) => {
     setAnswer(e.target.value);
-  }
+  };
   //
-  async function submitForm(answer) {
-    await new Promise((resolve) => setTimeout(resolve, 1500)); // Simulate a delay
-
-    if (answer.toLowerCase() !== "lima") {
-      throw new Error("Good guess but a wrong answer. Try again!");
-    }
-  }
+  let content;
   //
-  return (
-    <>
-      <p>
-        In which city is there a billboard that turns air into drinkable water?
-      </p>
 
-      <form onSubmit={handleSubmit}>
-        {/*  */}
-        <textarea
-          value={answer}
-          onChange={handleTextareaChange}
-          // what is this ?
-          disabled={status === "submitting"}
-        ></textarea>
-
+  if (answer == "lima") {
+    content = (
+      <>
+        <h3>Well done</h3>
+      </>
+    );
+  } else {
+    content = (
+      <>
+        <p>
+          In which city is there a billboard that turns air into drinkable
+          water?
+        </p>
+        <textarea onChange={answerHandler}></textarea>
         <br />
-        <button disabled={answer.length === 0 || status === "submitting"}>
-          Submit
-        </button>
-        {/*  */}
-        {error !== null && <p className="Error">{error.message}</p>}
-      </form>
-    </>
-  );
+        <button disabled={answer.length < 1}>submit</button>
+        <p>error</p>
+      </>
+    );
+  }
+  //
+  return <>{content}</>;
 };
 
 export default Home;
