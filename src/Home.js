52ec8e129c84898bdd7b4040cf33741faea9d339
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-03 00:06:54 +0530

    conditional form and button - with turnery syntax

--- src/Home.js
+++ src/Home.js
@@ -1,91 +1,56 @@
-//question ---
-// This form switches between two modes: in the editing mode, you see the inputs, and in the viewing mode, you only see the result. The button label changes between “Edit” and “Save” depending on the mode you’re in. When you change the inputs, the welcome message at the bottom updates in real time.
 import { useState } from "react";
 
-export default function EditProfile() {
+const Home = () => {
   //
-
-  const [firstName, setFirstName] = useState("f");
-  const [lastname, setLastName] = useState("l");
-
+  const [firstName, setFirstName] = useState("firstName");
+  const [lastName, setLastName] = useState("lastName");
   //
-  const [editStatus, setEditStatus] = useState(true);
+  const [isEditing, setisEditing] = useState(true);
   //
-  const firstnameHandler = (e) => {
-    setFirstName(e.target.value);
-  };
-  //
-  const lastnameHandler = (e) => {
-    setLastName(e.target.value);
+  const editButtonHandler = (e) => {
+    e.preventDefault();
+    setisEditing(false);
   };
   //
   const saveButtonHandler = (e) => {
     e.preventDefault();
-    setEditStatus(false);
+    setisEditing(true);
   };
   //
-  const editButtonHandler = (e) => {
-    e.preventDefault();
-    setEditStatus(true);
+  const firstNameHandler = (e) => {
+    setFirstName(e.target.value);
   };
   //
-  let content;
+  const lastNameHandler = (e) => {
+    setLastName(e.target.value);
+  };
   //
+  return (
+    <>
+      <label>First name: </label>
+      {isEditing ? (
+        <span>{firstName}</span>
+      ) : (
+        <input onChange={firstNameHandler} value={firstName} />
+      )}
 
-  if (editStatus) {
-    content = (
-      <>
-        <form>
-          <label>
-            First name:
-            <input value={firstName} onChange={firstnameHandler} />
-          </label>
-          <br />
-          <label>
-            Last name:
-            <input value={lastname} onChange={lastnameHandler} />
-          </label>
-          <br />
-          <button onClick={saveButtonHandler} type="submit">
-            Save Profile
-          </button>
-          <p>
-            <i>
-              Hello, {firstName} {lastname}!
-            </i>
-          </p>
-        </form>
-      </>
-    );
-  } else {
-    content = (
-      <>
-        <form>
-          <label>
-            First name:
-            {/* <input onChange={firstnameHandler} /> */}
-            {firstName}
-          </label>
-          <br />
-          <label>
-            Last name:
-            {/* <input onChange={lastnameHandler} /> */}
-            {lastname}
-          </label>
-          <br />
-          <button onClick={editButtonHandler} type="submit">
-            Edit Profile
-          </button>
-          <p>
-            <i>
-              Hello, {firstName} {lastname}!
-            </i>
-          </p>
-        </form>
-      </>
-    );
-  }
+      <br />
+      <label>Last name: </label>
 
-  //
-  return <>{content}</>;
-}
+      {isEditing ? (
+        <span>{lastName}</span>
+      ) : (
+        <input onChange={lastNameHandler} value={lastName} />
+      )}
+
+      <br />
+      {isEditing ? (
+        <button onClick={editButtonHandler}>Edit</button>
+      ) : (
+        <button onClick={saveButtonHandler}>save</button>
+      )}
+    </>
+  );
+};
+
+export default Home;
--- src/TaskList.js
+++ src/TaskList.js
@@ -1,47 +1,91 @@
-// import { useState } from "react";
+//question ---
+// This form switches between two modes: in the editing mode, you see the inputs, and in the viewing mode, you only see the result. The button label changes between “Edit” and “Save” depending on the mode you’re in. When you change the inputs, the welcome message at the bottom updates in real time.
+import { useState } from "react";
 
-export default function Picture() {
-  // const [imgTag, setImgTag] = useState(false);
+export default function EditProfile() {
   //
-  // if (imgTag) {
-  //   // setImgTag(!imgTag);
-  // } else {
-  //   setImgTag(!imgTag);
-  // }
-  //
-  let divClass = "";
-  let imgClass = "";
+
+  const [firstName, setFirstName] = useState("f");
+  const [lastname, setLastName] = useState("l");
 
   //
-  const divHandler = (e) => {
-    //
-    e.stopPropagation();
-    //
-    imgClass = "picture";
-    divClass = "background background--active";
-    //
-    console.log(imgClass, "imgClass divHandler");
-    console.log(divClass, "divClass divHandler");
-  };
-  const imgHandler = (e) => {
-    //
-    e.stopPropagation();
-    //
-    imgClass = "picture picture--active";
-    divClass = "background";
-    //
-    console.log(divClass, "divClass imgHandler");
-    console.log(imgClass, "imgClass imgHandler");
+  const [editStatus, setEditStatus] = useState(true);
+  //
+  const firstnameHandler = (e) => {
+    setFirstName(e.target.value);
   };
   //
-  return (
-    <div onClick={divHandler} className={imgClass}>
-      <img
-        onClick={imgHandler}
-        className={divClass}
-        alt="Rainbow houses in Kampung Pelangi, Indonesia"
-        src="https://i.imgur.com/5qwVYb1.jpeg"
-      />
-    </div>
-  );
+  const lastnameHandler = (e) => {
+    setLastName(e.target.value);
+  };
+  //
+  const saveButtonHandler = (e) => {
+    e.preventDefault();
+    setEditStatus(false);
+  };
+  //
+  const editButtonHandler = (e) => {
+    e.preventDefault();
+    setEditStatus(true);
+  };
+  //
+  let content;
+  //
+
+  if (editStatus) {
+    content = (
+      <>
+        <form>
+          <label>
+            First name:
+            <input value={firstName} onChange={firstnameHandler} />
+          </label>
+          <br />
+          <label>
+            Last name:
+            <input value={lastname} onChange={lastnameHandler} />
+          </label>
+          <br />
+          <button onClick={saveButtonHandler} type="submit">
+            Save Profile
+          </button>
+          <p>
+            <i>
+              Hello, {firstName} {lastname}!
+            </i>
+          </p>
+        </form>
+      </>
+    );
+  } else {
+    content = (
+      <>
+        <form>
+          <label>
+            First name:
+            {/* <input onChange={firstnameHandler} /> */}
+            {firstName}
+          </label>
+          <br />
+          <label>
+            Last name:
+            {/* <input onChange={lastnameHandler} /> */}
+            {lastname}
+          </label>
+          <br />
+          <button onClick={editButtonHandler} type="submit">
+            Edit Profile
+          </button>
+          <p>
+            <i>
+              Hello, {firstName} {lastname}!
+            </i>
+          </p>
+        </form>
+      </>
+    );
+  }
+
+  //
+  return <>{content}</>;
 }
