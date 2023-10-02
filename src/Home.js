7145126722098311fb6bac2e8940dfad408384ae
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-02 20:22:27 +0530

    conditional --  my try showing message/ error + button + form

--- src/Home.js
+++ src/Home.js
@@ -2,63 +2,55 @@ import { useState } from "react";
 
 const Home = () => {
   //
+
   const [answer, setAnswer] = useState("");
+  const [message, setMessage] = useState(null);
   const [status, setStatus] = useState("typing");
-  const [error, setError] = useState(null);
+
   //
-  const answerHandler = (e) => {
+  const inputHandler = (e) => {
     setAnswer(e.target.value);
   };
   //
-  async function handleFormSubmit() {
+  async function buttonHandler(e) {
     //
     setStatus("submitting");
+    //
     try {
       await submitForm(answer);
-      setStatus("success");
+      setMessage("Correct!");
     } catch (err) {
-      setStatus("typing");
-      setError(err);
       // console.log(err);
+      setMessage(err.message);
+      setStatus("typing");
     }
+    //
   }
-  //
-  let content;
-  //
 
-  if (status == "success") {
-    content = (
-      <>
-        <h3>Well done</h3>
-      </>
-    );
-  } else {
-    content = (
-      <>
-        <p>
-          In which city is there a billboard that turns air into drinkable
-          water?
-        </p>
-        <textarea onChange={answerHandler}></textarea>
-        <br />
-        <button
-          onClick={handleFormSubmit}
-          disabled={answer.length < 1 || status == "submitting"}
-        >
-          submit
-        </button>
-
-        {error !== null && <p>{error.message}</p>}
-      </>
-    );
-  }
   //
-  return <>{content}</>;
+  return (
+    <>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+
+      <textarea onChange={inputHandler}></textarea>
+
+      <button
+        onClick={buttonHandler}
+        disabled={answer.length < 1 || status == "submitting"}
+      >
+        submit
+      </button>
+
+      <p>{message}</p>
+    </>
+  );
 };
 
 //
-// Pretend it's hitting the network.
 function submitForm(answer) {
+  // Pretend it's hitting the network.
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       let shouldError = answer.toLowerCase() !== "lima";
