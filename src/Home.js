fe4e11a52fd54ab2e82c0dc928286dd374dfa231
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-04 00:12:52 +0530

    test

--- src/Home.js
+++ src/Home.js
@@ -1,71 +1,33 @@
 import { useState } from "react";
+import { initialTravelPlan } from "./TaskList.js";
 
-const initialItems = [
-  { title: "pretzels", id: 0 },
-  { title: "crispy seaweed", id: 1 },
-  { title: "granola bar", id: 2 },
-];
+function PlaceTree({ place }) {
+  const childPlaces = place.childPlaces;
+  return (
+    <li>
+      {place.title}
+      {childPlaces.length > 0 && (
+        <ol>
+          {childPlaces.map((place) => (
+            <PlaceTree key={place.id} place={place} />
+          ))}
+        </ol>
+      )}
+    </li>
+  );
+}
 
-const Home = () => {
-  const [array, setArray] = useState(initialItems);
-  const [position, setPosition] = useState(0);
-  //
-  let nowShow;
-
-  array.forEach((item) => {
-    if (item.id === position) {
-      //   console.log(item);
-      nowShow = item;
-      //   return item;
-    }
-  });
-
-  //   console.log(nowShow, "nowShow");
-
-  const buttonHandler = (sendItem) => {
-    setPosition(sendItem.id);
-  };
-  //
-  const inputHandler = (e, sentItem) => {
-    const mapValue = array.map((item) => {
-      if (item.id == sentItem.id) {
-        return { ...item, title: e.target.value };
-      } else {
-        return item;
-      }
-    });
-    //
-    setArray(mapValue);
-  };
-
+export default function TravelPlan() {
+  const [plan, setPlan] = useState(initialTravelPlan);
+  const planets = plan.childPlaces;
   return (
     <>
-      {array.map((item) => {
-        return (
-          <li key={item.id}>
-            {/* {item.title} */}
-
-            <input
-              onChange={(e) => {
-                inputHandler(e, item);
-              }}
-              value={item.title}
-            />
-
-            <button
-              onClick={() => {
-                buttonHandler(item);
-              }}
-            >
-              select
-            </button>
-          </li>
-        );
-      })}
-
-      <p>now showing:{nowShow.title} </p>
+      <h2>Places to visit</h2>
+      <ol>
+        {planets.map((place) => (
+          <PlaceTree key={place.id} place={place} />
+        ))}
+      </ol>
     </>
   );
-};
-
-export default Home;
+}
