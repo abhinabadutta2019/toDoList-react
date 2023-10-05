cc87afde393196efc25e09e020454b13524ce2b6
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-05 21:21:07 +0530

    list items showing in DOM, search field - onChange- showing in console

--- src/Home.js
+++ src/Home.js
@@ -4,12 +4,30 @@ import { foods } from "./Task.js";
 export default function FilterableList() {
   const [query, setQuery] = useState("");
 
+  //
+  function handleChange(e) {
+    setQuery(e.target.value);
+    console.log(query, "query");
+  }
+  //
+
   return (
     <>
+      <SearchBar query={query} onChange={handleChange} />
       <List items={foods} />
     </>
   );
 }
+
+//
+//
+const SearchBar = ({ query, onChange }) => {
+  return (
+    <>
+      <input value={query} onChange={onChange} />
+    </>
+  );
+};
 
 //
 //List component
@@ -19,7 +37,7 @@ const List = ({ items }) => {
       {items.map((item) => {
         return (
           <li key={item.id}>
-            {item.name} : {item.description}
+            {item.name} - - - {item.description}
           </li>
         );
       })}
