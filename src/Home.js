e534784cb9dd7bfc04b07d4beed1ea9d5a8c6975
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-05 23:06:03 +0530

    it helped me-- to understand -- the search code

--- src/Home.js
+++ src/Home.js
@@ -7,34 +7,56 @@ import { foods } from "./Task.js";
 
 //   const filterValue = items.filter((item) =>
 //     item.name.split(" ").some((word) => {
+//       //
+//       console.log(item.name.split(" "), "item.name");
+//       //
 //       return word.toLowerCase().startsWith(query);
 //     })
 //   );
 //   //
 //   return filterValue;
 // };
-
+//
 const filterItems = (items, query) => {
+  // Convert the query to lowercase for case-insensitive matching.
   query = query.toLowerCase();
 
+  // Create an array to store the filtered items.
   const filteredItems = items.filter((item) => {
-    const words = item.name.toLowerCase().split(" ");
+    // Split the item's name into an array of words.
+    const words = item.name.split(" ");
 
-    for (const word of words) {
-      if (word.startsWith(query)) {
-        return true;
-      }
-    }
-    return false;
+    // Log the array of words.
+    console.log(words, "Words in item.name");
+
+    // Check if any of the words start with the query.
+    const isMatching = words.some((word) => {
+      // Log the current word being checked.
+      // console.log(word, "Current word");
+
+      // Check if the lowercase word starts with the lowercase query.
+      return word.toLowerCase().startsWith(query);
+    });
+
+    //
+    console.log(isMatching, "isMatching");
+
+    // Return true if at least one word in the item's name matches the query.
+    return isMatching;
   });
 
+  // Return the filtered items.
   return filteredItems;
 };
-/////////////////////////////////////////////////
+
+//
 
 export default function FilterableList() {
   const [query, setQuery] = useState("");
   const results = filterItems(foods, query);
+
+  //
+  console.log(results, "results");
 
   //
   function handleChange(e) {
